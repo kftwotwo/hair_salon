4cd1ec8ab12fdee1e073772e5b8990aca51ead47
Kevin Finley <kftwotwo@gmail.com>
2016-12-09 12:11:06 -0800

    add initialize for first and last name

--- lib/clients.rb
+++ lib/clients.rb
@@ -1,3 +1,11 @@
-# class client
-#
-# end
+class Client
+  attr_reader :id, :first_name, :last_name
+
+  def initialize(attrs)
+    @first_name = attrs[:first_name]
+    @last_name = attrs[:last_name]
+    @id = attrs["id"].to_i
+  end
+
+
+end
--- spec/clients_spec.rb
+++ spec/clients_spec.rb
@@ -1,1 +1,15 @@
-# require('spec_helper')
+require('spec_helper')
+
+describe(Client) do
+  before :each do
+    @joe = Client.new({:first_name => "Kevin", :last_name => 'Finley'})
+  end
+
+  describe('#initialize') do
+    it "will initialize id, first_name, and last_name" do
+      expect(@joe.first_name).to eq 'Kevin'
+      expect(@joe.last_name).to eq 'Finley'
+    end
+  end
+
+end
