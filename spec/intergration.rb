f66058900f12b1d6777321c5b79b642ed19f66ab
Kevin Finley <kftwotwo@gmail.com>
2016-12-09 17:59:49 -0800

    add 3rd spec and pass

--- spec/intergration.rb
+++ spec/intergration.rb
@@ -31,3 +31,13 @@ describe('add clients to a stylist', {:type => :feature}) do
     expect(page).to have_content("John twotwo")
   end
 end
+
+describe('add stylists', {:type => :feature}) do
+  it('allows the user to add a stylist to list of stylists') do
+    visit('/stylists')
+    fill_in('first_name', :with => 'Table')
+    fill_in('last_name', :with => 'Chair')
+    click_button('Add Stylist')
+    expect(page).to have_content('Table Chair')
+  end
+end
